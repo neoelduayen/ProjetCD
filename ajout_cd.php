b2eb39574f86ab36cda13c5d8998f50767d0f25d
Néo Elduayen <nelduayen@univ-pau.fr>
2023-12-12 09:20:38 +0100

    ajout d'une fonction pour ajouter des CD dans la BD

---
+++ ajout_cd.php
@@ -0,0 +1,41 @@
+<?php
+$vide = "";
+if ($_POST['Titre'] != $vide && $_POST['Auteur'] != $vide){
+    if ($_POST['Genre'] != $vide && $_POST['Prix'] != $vide) {
+        $titre = $_POST['Titre'];
+        $auteur = $_POST['Auteur'];
+        $genre = $_POST['Genre'];
+        $prix = $_POST['Prix'];
+        $image = strtolower($titre);
+
+        $bdd= "nelduayen_bd"; // Base de données
+        $host= "lakartxela.iutbayonne.univ-pau.fr";
+        $user= "nelduayen_bd"; // Utilisateur
+        $pass= "nelduayen_bd"; // mp
+        $nomtable= "ProjetCD"; /* Connection bdd */
+        
+        $link=mysqli_connect($host,$user,$pass,$bdd) or die( "Impossible de se connecter à la base de données");
+        $query = "SELECT count(id) AS nb FROM $nomtable";
+        $result= mysqli_query($link,$query);
+        $row = mysqli_fetch_array($result);
+        $id=$row['nb'];
+        $id += 1;
+        $sql = "INSERT INTO ProjetCD (ID, Titre, Auteur, Genre, Prix, Image) VALUES ($id,'$titre','$auteur','$genre',$prix,'vignettes/$image.jpg')";
+        if (mysqli_query($link, $sql)) {
+            echo '<body onLoad="alert(\'CD ajouté à la base de données\')">';
+            echo '<meta http-equiv="refresh" content="0;URL=page_membre.php">';
+        }
+        else {
+            echo '<body onLoad="alert(\'Des données saisies sont invalides\')">';
+            echo '<meta http-equiv="refresh" content="0;URL=page_membre.php">';
+        }
+    }
+    else{
+        echo '<body onLoad="alert(\'Tout les champs ne sont pas remplis.\')">';
+        echo '<meta http-equiv="refresh" content="0;URL=page_membre.php">';
+    }
+}
+else{
+    echo '<body onLoad="alert(\'Tout les champs ne sont pas remplis.\')">';
+    echo '<meta http-equiv="refresh" content="0;URL=page_membre.php">';
+}
--- page_membre.php
+++ page_membre.php
@@ -7,16 +7,39 @@ if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) {
 // On teste pour voir si nos variables ont bien été enregistrées
 echo '<html>';
 echo '<head>';
-echo '<title>Page de notre section membre</title>';
+echo '<title>Back-office</title>';
 echo '</head>';
 
+$bdd= "nelduayen_bd"; // Base de données
+$host= "lakartxela.iutbayonne.univ-pau.fr";
+$user= "nelduayen_bd"; // Utilisateur
+$pass= "nelduayen_bd"; // mp
+$nomtable= "ProjetCD"; /* Connection bdd */
+
+$link=mysqli_connect($host,$user,$pass,$bdd) or die( "Impossible de se connecter à la base de
+données");
+$query = "SELECT * FROM $nomtable";
+    $result= mysqli_query($link,$query);
+    while ($donnees=mysqli_fetch_assoc($result)) {
+        $ch1=$donnees["ID"];
+        $ch2=$donnees["Titre"];
+        $ch3=$donnees["Prix"];
+        echo "<p>$ch1, $ch2, $ch3 €</p>";
+    }
 echo '<body>';
-echo 'Votre login est '.$_SESSION['login'].' et votre mot de passe est '.$_SESSION['pwd'].'.';
+// echo 'Votre login est '.$_SESSION['login'].' et votre mot de passe est '.$_SESSION['pwd'].'.';
+echo 'Bienvenue sur votre session.';
 echo '<br />';
+echo'<form action="ajout_cd.php" method="post">';
+echo 'Titre : <input type="text" name="Titre"><br />';
+echo 'Auteur : <input type="text" name="Auteur"><br />';
+echo 'Genre : <input type="text" name="Genre"><br />';
+echo 'Prix : <input type="text" name="Prix">';
+echo '<input type="submit" value="Ajouter"></form>';
 // On affiche un lien pour fermer notre session
 echo '<a href="./logout.php">Déconnexion</a>';
 }
 else {
-echo 'Les variables ne sont pas déclarées.';
+echo "Vous n'avez pas le droit de vous connecter à cette page.";
 }
 ?>
