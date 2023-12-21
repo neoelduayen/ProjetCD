569e9cb1a39d9ed074b0cd91351262b3b10cee38
Néo Elduayen <nelduayen@univ-pau.fr>
2023-12-21 19:12:18 +0100

    Ajout d'un système de panier

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-    $bdd= "nelduayen_bd"; // Base de données
-    $host= "lakartxela.iutbayonne.univ-pau.fr";
-    $user= "nelduayen_bd"; // Utilisateur
-    $pass= "nelduayen_bd"; // mp
-    $nomtable= "ProjetCD"; /* Connection bdd */
+    session_start();
+
+    $bdd = "nelduayen_bd"; // Base de données
+    $host = "lakartxela.iutbayonne.univ-pau.fr";
+    $user = "nelduayen_bd"; // Utilisateur
+    $pass = "nelduayen_bd"; // Mot de passe
+    $nomtable = "ProjetCD"; /* Table bdd */
 
     echo "<head>";
     echo    "<link rel='stylesheet' type='text/css' href='style.css' media='screen' />";
@@ -20,28 +22,51 @@
     echo                    "</picture>";
     echo                "</a>";
     echo            "</li>";
-    echo            "<li><a href='' >Panier</a></li>";
-    echo            "<li><a href='' >réalisations</a></li>";
-    echo            "<li><a href='' >services</a></li>";
-    echo            "<li><a href=''>contact</a></li>";
+    echo            "<li><a href='panier.php'>Panier</a></li>";
+    echo            "<li><a href='realisations.php'>Réalisations</a></li>";
+    echo            "<li><a href='services.php'>Services</a></li>";
+    echo            "<li><a href='contact.php'>Contact</a></li>";
     echo        "</ul>";
     echo    "</nav>";
     echo "</header>";
-    
-    $link=mysqli_connect($host,$user,$pass,$bdd) or die( "Impossible de se connecter à la base de
-    données");
+
+    $link = mysqli_connect($host, $user, $pass, $bdd) or die("Impossible de se connecter à la base de données");
     $query = "SELECT * FROM $nomtable";
-    $result= mysqli_query($link,$query);
-    while ($donnees=mysqli_fetch_assoc($result)) {
-        $ch1=$donnees["Image"];
-        $ch2=$donnees["Titre"];
-        $ch3=$donnees["Auteur"];
-        echo "<picture>";
-        echo    "<img src='$ch1' alt='' />";
-        echo "</picture>";
-        echo "<h1> $ch2 </h1>";
-        echo "<h2> $ch3 </h2>";
+    $result = mysqli_query($link, $query);
+
+    if (!isset($_SESSION['panier'])) {
+        $_SESSION['panier'] = array();
+    }
+
+    while ($donnees = mysqli_fetch_assoc($result)) {
+        $ch1 = $donnees["Image"];
+        $ch2 = $donnees["Titre"];
+        $ch3 = $donnees["Auteur"];
+        $id = $donnees["ID"];
+
+        echo "<form method='post' action=''>"; 
+        echo    "<input type='hidden' name='action' value='ajouter'>";
+        echo    "<input type='hidden' name='id' value='$id'>";
+        echo    "<picture>";
+        echo        "<img src='$ch1' alt='' />";
+        echo    "</picture>";
+        echo    "<h1>$ch2</h1>";
+        echo    "<h2>$ch3</h2>";
+        echo    "<label for='quantite'>Quantité :</label>";
+        echo    "<input type='number' name='quantite' value='1' min='1'>";
+        echo    "<input type='submit' value='Ajouter au panier'>";
+        echo "</form>";
         echo '<br />';
     }
-    echo "</body>"
+
+    if(isset($_POST['action']) && $_POST['action']=="ajouter"){
+        $id = $_POST['id'];
+        $quantite = isset($_POST['quantite']) ? intval($_POST['quantite']) : 1;
+
+        for ($i = 0; $i < $quantite; $i++) {
+            array_push($_SESSION['panier'], $id);
+        }
+    }
+
+    echo "</body>";
 ?>
--- panier.php
+++ panier.php
@@ -1,1 +1,97 @@
-<h1>Bienvenue sur votre panier</h1>
+<?php
+session_start();
+
+$bdd = "nelduayen_bd"; // Base de données
+$host = "lakartxela.iutbayonne.univ-pau.fr";
+$user = "nelduayen_bd"; // Utilisateur
+$pass = "nelduayen_bd"; // Mot de passe
+$nomtable = "ProjetCD"; /* Table bdd */
+
+echo "<head>";
+echo    "<link rel='stylesheet' type='text/css' href='style.css' media='screen' />";
+echo "</head>";
+
+echo "<body>";
+echo "<header>";
+echo    "<nav>";
+echo        "<ul>";
+echo            "<li>";
+echo                "<a href='index.php' >";
+echo                    "<picture>";
+echo                        "<img src='images/logo.png' alt='' />";
+echo                    "</picture>";
+echo                "</a>";
+echo            "</li>";
+echo            "<li><a href='panier.php'>Panier</a></li>";
+echo            "<li><a href='realisations.php'>Réalisations</a></li>";
+echo            "<li><a href='services.php'>Services</a></li>";
+echo            "<li><a href='contact.php'>Contact</a></li>";
+echo        "</ul>";
+echo    "</nav>";
+echo "</header>";
+
+// Affichage du panier
+if (isset($_SESSION['panier']) && count($_SESSION['panier']) > 0) {
+    echo "<h1>Votre Panier</h1>";
+
+    // Se connecter à la base de données
+    $link = mysqli_connect($host, $user, $pass, $bdd) or die("Impossible de se connecter à la base de données");
+
+    // Récupérer les informations sur les CDs dans le panier
+    $panierIds = array_unique($_SESSION['panier']); // Pour éviter les doublons
+    $inClause = implode(',', $panierIds);
+    $query = "SELECT * FROM $nomtable WHERE ID IN ($inClause)";
+    $result = mysqli_query($link, $query);
+
+    // Afficher les CDs dans le panier
+    $totalPrix = 0;
+    while ($donnees = mysqli_fetch_assoc($result)) {
+        $id = $donnees["ID"];
+        $ch1 = $donnees["Image"];
+        $ch2 = $donnees["Titre"];
+        $prix = $donnees["Prix"];
+        $quantite = array_count_values($_SESSION['panier'])[$id];
+
+        $sousTotal = $prix * $quantite;
+        $totalPrix += $sousTotal;
+
+        echo "<div class='cd-panier'>";
+        echo    "<picture>";
+        echo        "<img src='$ch1' alt='' />";
+        echo    "</picture>";
+        echo    "<h2>$ch2</h2>";
+        echo    "<p>Quantité: $quantite</p>";
+        echo    "<p>Prix unitaire: $prix €</p>";
+        echo    "<p>Sous-total: $sousTotal €</p>";
+        echo    "<form method='post' action=''>";
+        echo        "<input type='hidden' name='action' value='supprimer'>";
+        echo        "<input type='hidden' name='id' value='$id'>";
+        echo        "<input type='submit' value='Supprimer du panier'>";
+        echo    "</form>";
+        echo "</div>";
+    }
+
+    
+    // Afficher le prix total
+    echo "<div class='total-panier'>";
+    echo    "<p>Prix total: $totalPrix €</p>";
+    echo "</div>";
+
+    // Bouton pour aller vers la page de paiement
+    echo "<form method='get' action='paiement.php'>";
+    echo    "<input type='submit' value='Aller vers le paiement'>";
+    echo "</form>";
+} 
+else {
+    echo "<p>Votre panier est vide.</p>";
+}
+
+// Gestion de la suppression du panier
+if (isset($_POST['action']) && $_POST['action'] == "supprimer") {
+    $id = $_POST['id'];
+    $_SESSION['panier'] = array_diff($_SESSION['panier'], array($id));
+    echo '<meta http-equiv="refresh" content="0;URL=panier.php">';
+}
+
+echo "</body>";
+?>
